ecb8fe874222ce17a01d82c3e4b35b7e5abb4f21
Tanvir Tanjum Shourav <39719570+tanvirtanjum@users.noreply.github.com>
2021-01-30 19:04:38 +0600

    Initial commit

---
+++ NP++/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using NP__.Operation;
+
+namespace NP__
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenSave fl = new OpenSave();
+            string write = fl.OF();
+            writableBox.Text =write + "";
+        }
+
+        private void SaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenSave fl = new OpenSave();
+            fl.SF(writableBox.Text);
+        }
+
+        private void ClearBtn_Click(object sender, RoutedEventArgs e)
+        {
+            writableBox.Clear();
+        }
+    }
+}
---
+++ NP++/Operation/OpenSave.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace NP__.Operation
+{
+    internal class OpenSave
+    {
+        readonly OpenFileDialog ofd;
+        readonly SaveFileDialog sfd;
+        string txtwrite="";
+        internal string OF()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            ofd.Filter = "Text file (*.txt)|*.txt|C file (*.c)|*.c|C++ file (*.cpp)|*.cpp|C# file (*.cs)|*.cs|All file (*.*)|*.*";
+            ofd.DefaultExt = "txt";
+            ofd.FilterIndex = 5;
+
+            if (ofd.ShowDialog() == true)
+            {
+                try { txtwrite = File.ReadAllText(ofd.FileName);}
+                catch (Exception ex) { throw ex; }
+                return txtwrite;
+            }
+            return txtwrite;
+        }
+
+        internal void SF(string swrite)
+        {
+            while (swrite != "")
+            {
+                SaveFileDialog sfd = new SaveFileDialog();
+                {
+                    //sfd.FileName = WriteTB.Text.Substring(0,1);
+                    sfd.FileName = "*";
+                    sfd.Filter = "Text file (*.txt)|*.txt|C file (*.c)|*.c|C++ file (*.cpp)|*.cpp|C# file (*.cs)|*.cs|All file (*.*)|*.*";
+                    //sfd.InitialDirectory = @"C:\"; 
+                    //sfd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                    sfd.DefaultExt = "txt";
+                    sfd.FilterIndex = 5;
+                    sfd.RestoreDirectory = true;
+                    sfd.Title = "Save File";
+                    sfd.CheckFileExists = false;
+                    sfd.CheckPathExists = false;
+                    sfd.OverwritePrompt = true;
+                    sfd.ValidateNames = true;
+                };
+
+                while (sfd.ShowDialog() == true)
+                {
+                    try
+                    {
+                        File.WriteAllText(sfd.FileName, swrite);
+                    }
+                    catch (Exception ex)
+                    {
+                        throw ex;
+                    }
+                    break;
+                }
+                break;
+            }
+        }
+    }
+}
